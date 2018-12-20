e5c7039d768d20bd73e0f216206d69ce5094de70
Jason Matthews <jmatty1983@gmail.com>
2018-12-19 23:20:22 -0500

    Fix multi select on table

--- ui/components/DisplayTable.js
+++ ui/components/DisplayTable.js
@@ -1,25 +1,83 @@
-import React from "react";
+import React, { useState } from "react";
 import Table from "@material-ui/core/Table";
 import TableBody from "@material-ui/core/TableBody";
 import TableCell from "@material-ui/core/TableCell";
 import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
+import Checkbox from "@material-ui/core/Checkbox";
 
 const DisplayTable = props => {
-  const { headers, data } = props;
+  const [selectedRows, setSelectedRows] = useState([]);
+  const { headers, data, multiSelect, onSelectChange } = props;
+
+  const handleSelectAll = (_, checked) => {
+    const rows = checked ? data.map((_, i) => i) : [];
+    setSelectedRows(rows);
+    onSelectChange(rows);
+  };
+
+  const handleSelect = (checked, i) => {
+    const rows = checked
+      ? [...selectedRows, i]
+      : selectedRows.filter(r => r !== i);
+    onSelectChange(rows);
+    setSelectedRows(rows);
+  };
+
+  const handleRowClick = i => {
+    if (!multiSelect) {
+      return;
+    }
+
+    handleSelect(!selectedRows.includes(i), i);
+  };
 
   return (
     <Table>
       <TableHead>
         <TableRow>
-          {headers.map(header => (
-            <TableCell key={header}>{header}</TableCell>
-          ))}
+          <>
+            {(() => {
+              if (multiSelect) {
+                return (
+                  <TableCell>
+                    <Checkbox
+                      indeterminate={
+                        selectedRows.length > 0 &&
+                        selectedRows.length < data.length
+                      }
+                      checked={selectedRows.length === data.length}
+                      onChange={handleSelectAll}
+                    />
+                  </TableCell>
+                );
+              }
+            })()}
+            {headers.map(header => (
+              <TableCell key={header}>{header}</TableCell>
+            ))}
+          </>
         </TableRow>
       </TableHead>
       <TableBody>
         {data.map((row, index) => (
-          <TableRow key={index}>
+          <TableRow
+            hover={true}
+            key={index}
+            onClick={() => handleRowClick(index)}
+          >
+            {(() => {
+              if (multiSelect) {
+                return (
+                  <TableCell>
+                    <Checkbox
+                      checked={selectedRows.includes(index)}
+                      onChange={(_, checked) => handleSelect(checked, index)}
+                    />
+                  </TableCell>
+                );
+              }
+            })()}
             {row.map((cell, cellIndex) => (
               <TableCell key={cellIndex}>{cell}</TableCell>
             ))}
--- ui/pages/PaperTrader.js
+++ ui/pages/PaperTrader.js
@@ -1,31 +1,41 @@
 import React, { useEffect, useState } from "react";
+
 import CircularProgress from "@material-ui/core/CircularProgress";
 import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
+
+import styled from "styled-components";
 
 import DisplayTable from "../components/DisplayTable";
 import Layout from "../components/Layout";
 
+const TableHeading = styled.div`
+  padding-top: 10px;
+  padding-left: 10px;
+`;
+
 const PaperTrader = () => {
-  const headers = ["Name", "Win %"];
-  const [data, setData] = useState([]);
   const [tableData, setTableData] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [rowsSelected, setRowsSelected] = useState([]);
+
+  const headers = ["Name", "Win %"];
 
   const fetchData = async () => {
     const response = await fetch(`/api/genomes`);
     const json = await response.json();
-    setData(json);
 
-    const formatted = json.map(d => [
-      d.name
-        .split("\\")
-        .pop()
-        .split("_")
-        .slice(0, 5)
-        .join(" "),
-      `${Math.round(d.data.testStats.winRate * 100) / 100}%`
+    const formatted = json.map((d, i) => [
+      <div onClick={() => console.log(d)}>
+        {d.name
+          .split("\\")
+          .pop()
+          .split("_")
+          .slice(0, 5)
+          .join(" ")}
+      </div>,
+      `${Math.round(d.data.testStats.winRate * 10000) / 100}%`
     ]);
-
     setTableData(formatted);
     setLoading(false);
   };
@@ -41,7 +51,19 @@ const PaperTrader = () => {
         {loading ? (
           <CircularProgress />
         ) : (
-          <DisplayTable headers={headers} data={tableData} />
+          <>
+            <TableHeading>
+              <Typography variant="h6">
+                Genomes - Selected: {rowsSelected.length}
+              </Typography>
+            </TableHeading>
+            <DisplayTable
+              multiSelect={true}
+              onSelectChange={setRowsSelected}
+              headers={headers}
+              data={tableData}
+            />
+          </>
         )}
       </Paper>
     </Layout>
